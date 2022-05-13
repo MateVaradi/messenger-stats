06795da25efca552b73c62424424f055c75818a3
matevaradain <93525955+matevaradain@users.noreply.github.com>
2022-05-13 10:51:00 +0200

    Add script

--- analyze.py
+++ analyze.py
@@ -1,37 +1,27 @@
 # Imports
-import networkx as nx
-import seaborn as sns
-import matplotlib.pyplot as plt
+import sys
 from data_cleaning import prepare_data
-from aesthetics import set_fonts, set_aesthetics, set_colors
+from aesthetics import set_aesthetics, set_colors
 from plotting import MessengerReport
 
 
 def run(conversation):
     # Load and clean data
-    df = prepare_data("data/{conversation}")
+    df = prepare_data(f"data/{conversation}/")
 
-    # Font settings
-    set_fonts(fontsize=13)
     # Aesthetic settings
     set_aesthetics()
     # Color settings
     members = sorted(df.sender_name.unique().tolist())
-    num_members = len(members)
     color_dict = set_colors(members)
 
     reporter = MessengerReport(data=df, color_dict=color_dict, folder_name=conversation)
     reporter.pdf_report()
 
-    # TODO:
-    # add message samples to pdf
-    # finalize pdf report
-    # figure out color_dict/palette
-
-    # run for 2 other group chats
-
-    # write blogpost
-
 
 if __name__ == "__main__":
-    run("data/technokratak3")
+    if len(sys.argv) > 1:
+        conversation = sys.argv[1]
+    else:
+        raise ValueError("Add the name of the conversation as input")
+    run(conversation)
