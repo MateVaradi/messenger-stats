4d3f0de6772977016f337d7a23ca2dbcf0cbdb9a
matevaradain <93525955+matevaradain@users.noreply.github.com>
2022-05-13 10:51:17 +0200

    Add utils

---
+++ utils.py
@@ -0,0 +1,16 @@
+""" Helpers """
+
+
+def nth_repl_all(s, sub, repl, nth):
+    find = s.find(sub)
+    # loop util we find no match
+    i = 1
+    while find != -1:
+        # if i  is equal to nth we found nth matches so replace
+        if i == nth:
+            s = s[:find] + repl + s[find + len(sub) :]
+            i = 0
+        # find + len(sub) + 1 means we start after the last match
+        find = s.find(sub, find + len(sub) + 1)
+        i += 1
+    return s
